1a4a7f36c31f35cf373437dda14b55a01352ac91
Manny2023 <120243119+Manny2023@users.noreply.github.com>
2023-05-18 02:27:41 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,7 @@
-// Instalar el Service Worker
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('mi-pwa-cache').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/css/styles.css',
-        '/js/main.js',
-        '/img/logo.png'
-      ]);
-    })
-  );
-});
-
-// Activar el Service Worker
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.filter(function(cacheName) {
-          // Eliminar las cachés antiguas si existen
-          return cacheName.startsWith('mi-pwa-cache') && cacheName !== 'mi-pwa-cache';
-        }).map(function(cacheName) {
-          return caches.delete(cacheName);
-        })
-      );
-    })
-  );
-});
-
-// Interceptar las solicitudes de red y buscar en la caché
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
-});
+<script>
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('worker.js?'+Math.random()+'')
+  });
+}
+</script>
